5fbdbfe878014444040828c129359b523da5a70c
n9dyu <n9dyus@gmail.com>
2025-10-19 23:55:27 +0800

    Error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate, Navigate } from 'react-router-dom';
 import { useState } from 'react';
 
 import Login from './components/Login.jsx'
